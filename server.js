9e161a831577e3746fea02607394517e14452694
Glitch (hello-webpage) <none>
2020-04-22 21:56:15 +0000

    ✈️🗃 Checkpoint ./server.js:9838890/63 ./public/index.html:9838890/59

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const server = require("http").createServer(app);
+const port = process.env.PORT || 3000;
+
+server.listen(port, () => {
+  console.log("listening!");
+})
+
+
+
+console.log("hello node.js!")
