869d0cdd4da576afcc43bbbd181479a4371c45be
Glitch (hello-webpage) <none>
2020-04-22 22:07:15 +0000

    😀👝 Checkpoint ./server.js:9838890/304 ./public/index.html:9838890/510 ./public/sketch.js:9838890/147 ./public/style.css:9838890/72 ./package.json:9838890/22

---
+++ public/sketch.js
@@ -0,0 +1,12 @@
+const socket = io();
+
+io.
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  ellipse(mouseX, mouseY, 40);
+}
--- server.js
+++ server.js
@@ -3,10 +3,21 @@ const app = express();
 const server = require("http").createServer(app);
 const port = process.env.PORT || 3000;
 
+const io = require("socket.io")(server);
+
 server.listen(port, () => {
   console.log("listening!");
 })
 
+app.use(express.static('public'));
 
 
+let numGuests = 0;
+
+io.on("connection", (socket) => {
+  numGuests++;
+  console.log(`you are ${numGuests}th guest!`);
+  
+  socket.emit("hello", {numGuests: numGuests});
+})
 console.log("hello node.js!")
