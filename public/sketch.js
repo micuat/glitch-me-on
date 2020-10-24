c000ae1800674b77b628b51c8741e5a4b61becd4
Glitch (hello-webpage) <none>
2020-10-24 19:46:45 +0000

    💠⭐️ Checkpoint ./public/sketch.js:9838890/116

--- public/sketch.js
+++ public/sketch.js
@@ -49,7 +49,9 @@ var v = new Vue({
     src(s0)
       .color(-1, Math.random() * 2, 1)
       .colorama()
-      .out();`,
+      .out();
+    render(o0);
+`,
 
         feedback: `
     src(o1)
@@ -64,21 +66,27 @@ var v = new Vue({
     src(s0)
       .thresh()
       .diff(src(o0).scrollX(0.001))
-      .out();`,
+      .out();
+    render(o0);
+`,
 
         hueCamera: `
     src(o0).modulateHue(src(o0).scale(1.01), 8).layer(src(s0).luma(0.3).hue(()=>time/10))
-      .out();`,
+      .out();
+    render(o0);
+`,
 
       //here
       fbkCam: `
     src(o0)
       .saturate(1.05)
       .layer(src(s0).luma())
-      .out();`,  
+      .out();
+    render(o0);
+`,  
         
       boxOnTop: `
-    src(o1).layer(shape(4,0.5,0.01).luma()).out(o1);
+    src(o0).layer(osc(60,0.1,2).mask(shape(4,0.5,0.01)).luma()).out(o1);
     render(o1)`,
 
       }
