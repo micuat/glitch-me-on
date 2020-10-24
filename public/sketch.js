bafb0e06f27fb956bb0dc0e394f0d2513c6d9b1b
Glitch (hello-webpage) <none>
2020-10-24 19:00:52 +0000

    👹🍟 Checkpoint ./public/sketch.js:9838890/1764:22042091/18 ./public/index.html:9838890/373

--- public/sketch.js
+++ public/sketch.js
@@ -37,53 +37,43 @@ new Vue({
   el: "#hello-world-app",
   data() {
     return {
-      objectItems: {
-        key1: "item1",
-        key2: "item 2",
-        key3: "item3"
-      }
-    };
-  }
-});
-var funcs = {
-  oscillate: () => {
-    osc(20)
+funcs: {
+  oscillate: `osc(20)
       .rotate(0, 0.1)
       .modulate(osc())
-      .out();
-  },
+      .out();`,
 
-  kaleido: () => {
+  kaleido: `
     osc(10, 0.1, 0.8)
       .rotate(0, 0.1)
       .kaleid()
       .color(-1, 1)
       .out();
-  },
+  `,
   // create functions to use with buttons
-  useCamera: () => {
+  useCamera: `
     s0.initCam();
     src(s0)
       .color(-1, Math.random() * 2, 1)
       .colorama()
-      .out();
-  },
+      .out();`,
 
-  feedback: () => {
+  feedback: `
     src(o1)
-      .layer(src(o0).mask(shape(4, 0.4, 0)))
+      .layer(src(o0).mask(shape(4, [0.4,0,1].fast(0.3), 0)))
       .scrollX([0.005, -0.005])
       .scrollY(0.005)
       .out(o1);
 
-    render(o1);
-  },
+    render(o1);`,
 
-  useCamera1: () => {
+  useCamera1: `
     s0.initCam();
     src(s0)
       .thresh()
       .diff(src(o0).scrollX(0.001))
-      .out();
-  }
-};
+      .out();`
+}
+  }}
+});
+
