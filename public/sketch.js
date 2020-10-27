67cd3ec442cd5ea4eb701e4d858c65caa7329218
Glitch (hello-webpage) <none>
2020-10-27 08:33:09 +0000

    🍛🐬 Checkpoint ./public/sketch.js:9838890/4844

--- public/sketch.js
+++ public/sketch.js
@@ -13,72 +13,101 @@ var v = new Vue({
     emit: function(ev, key) {
       console.log(ev); // this is the event
       console.log(key); // i is index of v-for
-      eval(this.funcs[key]);
+      this.funcs[key]();
       socket.emit("func", key);
     }
   },
   data() {
     return {
       funcs: {
-        oscillate: `osc(20)
-      .rotate(0, 0.1)
-      .modulate(osc())
-      .out();`,
+        oscillate: () =>
+          osc(20)
+            .rotate(0, 0.1)
+            .modulate(osc())
+            .out(),
 
-        kaleido: `
-    osc(10, 0.1, 0.8)
-      .rotate(0, 0.1)
-      .kaleid()
-      .color(-1, 1)
-      .out();
-  `,
+        kaleido: () =>
+          osc(10, 0.1, 0.8)
+            .rotate(0, 0.1)
+            .kaleid()
+            .color(-1, 1)
+            .out(),
         // create functions to use with buttons
-        useCamera: `
-    src(s0)
-      .color(-1, Math.random() * 2, 1)
-      .colorama()
-      .out();
-    render(o0);
-`,
+        useCamera: () => {
+          src(s0)
+            .color(-1, Math.random() * 2, 1)
+            .colorama()
+            .out();
+          render(o0);
+        },
 
-        feedback: `
-    src(o1)
-      .layer(src(o0).mask(shape(4, [0.4,0,1].fast(0.3), 0)))
-      .scrollX([0.005, -0.005])
-      .scrollY(0.005)
-      .out(o1);
+        feedback: () => {
+          src(o1)
+            .layer(src(o0).mask(shape(4, [0.4, 0, 1].fast(0.3), 0)))
+            .scrollX([0.005, -0.005])
+            .scrollY(0.005)
+            .out(o1);
 
-    render(o1);`,
+          render(o1);
+        },
 
-        useCamera1: `
-    src(s0)
-      .thresh()
-      .diff(src(o0).scrollX(0.001))
-      .out();
-    render(o0);
-`,
+        useCamera1: () => {
+          src(s0)
+            .thresh()
+            .diff(src(o0).scrollX(0.001))
+            .out();
+          render(o0);
+        },
 
-        hueCamera: `
-    src(o0).modulateHue(src(o0).scale(1.01), 8).layer(src(s0).luma(0.3).hue(()=>time/10))
-      .out();
-    render(o0);
-`,
+        hueCamera: () => {
+          src(o0)
+            .modulateHue(src(o0).scale(1.01), 8)
+            .layer(
+              src(s0)
+                .luma(0.3)
+                .hue(() => time / 10)
+            )
+            .out();
+          render(o0);
+        },
 
         //here
-        fbkCam: `
-    src(o0)
-      .saturate(1.05)
-      .layer(src(s0).luma())
-      .out();
-    render(o0);
-`,
+        fbkCam: () => {
+          src(o0)
+            .saturate(1.05)
+            .layer(src(s0).luma())
+            .out();
+          render(o0);
+        },
 
-        boxOnTop: `
-    src(o0).layer(osc(60,0.1,2).mask(shape(4,0.5,0.01)).luma()).out(o1);
-    render(o1)`,
-        head: `
-    src(o0).layer(gradient().repeat(10,10).colorama(0.5).modulate(osc(3).rotate(0.5)).modulateScale(osc(2).rotate(0.5),-0.5).mask(shape(200,0.2,0.1).scale(1.7,0.4).scrollY(0.08)).luma(0.01)).out(o1);
-    render(o1)`
+        boxOnTop: () => {
+          src(o0)
+            .layer(
+              osc(60, 0.1, 2)
+                .mask(shape(4, 0.5, 0.01))
+                .luma()
+            )
+            .out(o1);
+          render(o1);
+        },
+        head: () => {
+          src(o0)
+            .layer(
+              gradient()
+                .repeat(10, 10)
+                .colorama(0.5)
+                .modulate(osc(3).rotate(0.5))
+                .modulateScale(osc(2).rotate(0.5), -0.5)
+                .mask(
+                  shape(200, 0.2, 0.1)
+                    .scale(1.7, 0.4)
+                    .scrollY(0.08)
+                )
+                .luma(0.01)
+            )
+            .out(o1);
+          render(o1);
+        }
       }
     };
   }
@@ -92,6 +121,6 @@ socket.on("hello", data => {
 
 socket.on("func", data => {
   if (v.funcs[data] != undefined) {
-    eval(v.funcs[data]);
+    v.funcs[data]();
   }
 });
